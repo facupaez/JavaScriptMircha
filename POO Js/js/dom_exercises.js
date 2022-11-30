0251d5b7542c1eaad02bfc9182c6726d749c8ded
facupaez <facu.pz10@gmail.com>
2022-11-29 23:20:23 -0300

    Mover bola con flechas del teclado

--- POO Js/js/dom_exercises.js
+++ POO Js/js/dom_exercises.js
@@ -1,12 +1,12 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
-import { shorcuts } from "./dom/teclado.js";
+import { moveBall, shorcuts } from "./dom/teclado.js";
 const d = document;
 
-d.addEventListener("DOMContentLoaded", e => {
-    hamburgerMenu(".panel-btn", ".panel", ".menu a");
-    digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
-    alarm("../assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+d.addEventListener("DOMContentLoaded", (e) => {
+  hamburgerMenu(".panel-btn", ".panel", ".menu a");
+  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
+  alarm("../assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
 });
 
 /* teclado 3 posiciones: 
@@ -14,6 +14,7 @@ keyup cuando soltamos la tecla,
 keydown cuando presionamos la tecla,
 keypress mientras presionamos la tecla */
 
-d.addEventListener("keyup", (e)=>{
-    shorcuts(e);
-})
+d.addEventListener("keydown", (e) => {
+  shorcuts(e);
+  moveBall(e, ".ball", ".stage");
+});
