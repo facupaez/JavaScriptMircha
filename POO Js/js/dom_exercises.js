3db570ee41c6579bb90dd90054307b958ad3ba29
facupaez <facu.pz10@gmail.com>
2022-11-30 21:03:35 -0300

    Reloj cuenta regresiva

--- POO Js/js/dom_exercises.js
+++ POO Js/js/dom_exercises.js
@@ -1,3 +1,4 @@
+import countdown from "./dom/cuenta_regresiva.js";
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { moveBall, shorcuts } from "./dom/teclado.js";
@@ -7,13 +8,13 @@ d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("../assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+  countdown("countdown", "February 15, 2023", "Feliz Cumpleaños!");
 });
 
 /* teclado 3 posiciones: 
 keyup cuando soltamos la tecla,
 keydown cuando presionamos la tecla,
 keypress mientras presionamos la tecla */
-
 d.addEventListener("keydown", (e) => {
   shorcuts(e);
   moveBall(e, ".ball", ".stage");
