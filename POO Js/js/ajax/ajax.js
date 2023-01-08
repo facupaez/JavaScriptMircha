7108c2c362bdc863918f41a704092faae848ec84
facupaez <facu.pz10@gmail.com>
2023-01-08 02:49:03 -0300

    AJAX: API Fetch

--- POO Js/js/ajax/ajax.js
+++ POO Js/js/ajax/ajax.js
@@ -7,11 +7,11 @@
     if (xhr.readyState != 4) return;
 
     if (xhr.status >= 200 && xhr.status < 300) {
-      console.log("Éxito");
+      //console.log("Éxito");
       /*       console.log(xhr.responseText);
       $xhr.innerHTML = xhr.responseText; */
       let json = JSON.parse(xhr.responseText);
-      console.log(json);
+      //console.log(json);
 
       json.forEach((el) => {
         const $li = document.createElement("li");
@@ -21,7 +21,7 @@
 
       $xhr.appendChild($fragment);
     } else {
-      console.log("Error");
+      // console.log("Error");
       let message = xhr.statusText || "Ocurrió un error en la petición";
       $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
     }
@@ -30,4 +30,34 @@
   xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
 
   xhr.send();
+})();
+
+(() => {
+  const $fetch = document.getElementById("fetch"),
+    $fragment = document.createDocumentFragment();
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then((json) => {
+      // console.log(json);
+      //$fetch.innerHTML = json;
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+        $fragment.appendChild($li);
+      });
+
+      $fetch.appendChild($fragment);
+    })
+    .catch((err) => {
+      //console.log(err);
+      let message = err.statusText || "Ocurrió un error en la petición";
+      $fetch.innerHTML = `Error ${fetch.status}: ${message}`;
+    })
+    .finally(() =>
+      console
+        .log
+        //  "Esto se ejecutará independientemente del resultado de la promesa fetch"
+        ()
+    );
 })();
